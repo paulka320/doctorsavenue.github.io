0c86f743e9dc92c6514ccf8059b3fa899ee111e3
paul320 <paulacou4@gmail.com>
2026-02-03 10:53:05 +0300

    Restore Swiper initialization code

--- script.js
+++ script.js
@@ -5,38 +5,3 @@ const swiper = new Swiper('.swiper-container', {
     autoplay: { delay: 5000 },
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  const counters = document.querySelectorAll(".count");
-  let started = false;
-
-  const startCounting = () => {
-    if (started) return;
-    started = true;
-
-    counters.forEach(counter => {
-      const target = +counter.dataset.target;
-      let count = 0;
-      const increment = target / 200;
-
-      const update = () => {
-        if (count < target) {
-          count += increment;
-          counter.innerText = Math.floor(count).toLocaleString();
-          requestAnimationFrame(update);
-        } else {
-          counter.innerText = target.toLocaleString();
-        }
-      };
-
-      update();
-    });
-  };
-
-  const observer = new IntersectionObserver(entries => {
-    if (entries[0].isIntersecting) {
-      startCounting();
-    }
-  }, { threshold: 0.5 });
-
-  observer.observe(document.querySelector(".impact"));
-});
