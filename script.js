d1b29a2426916e21b62b6fcbd7905ad9195c076d
paul320 <paulacou4@gmail.com>
2026-02-03 10:16:06 +0300

    Implement dynamic counter updates in script.js
    Added counter functionality to update displayed numbers dynamically.

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ const swiper = new Swiper('.swiper-container', {
     navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
     autoplay: { delay: 5000 },
 });
+const counters = document.querySelectorAll('.count');
+
+counters.forEach(counter => {
+  counter.innerText = '0';
+  const updateCount = () => {
+    const target = +counter.getAttribute('data-target');
+    const current = +counter.innerText;
+    const increment = Math.ceil(target / 200); // adjust speed
+
+    if (current < target) {
+      counter.innerText = current + increment;
+      setTimeout(updateCount, 20);
+    } else {
+      counter.innerText = target.toLocaleString();
+    }
+  };
+  updateCount();
+});
